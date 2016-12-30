0a5dbb5a8b014d735a41bbf04df0d5fadf80731c
todeilatiberia <todeilatiberia@gmail.com>
2016-12-31 00:57:43 +0200

    Data Base
    For final year project

---
+++ wfdb-app-toolbox-master/src/org/physionet/wfdb/SystemSettings.java
@@ -0,0 +1,151 @@
+package org.physionet.wfdb;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
+
+public class SystemSettings {
+
+	static String fsep= System.getProperty("file.separator");
+
+	public static void loadCurl(Boolean customArch){
+
+		if(getOsName().contains("windows")){
+			//On Windows, load the shipped curl library
+			System.load(SystemSettings.getWFDB_NATIVE_BIN(customArch) 
+					+ "\\bin\\libcurl-4.dll" );
+		}else if(getOsName().contains("mac")){
+			String libCurlName= SystemSettings.getWFDB_NATIVE_BIN(customArch) 
+					+ "bin/libcurl.4.dylib";
+			SecurityManager security = System.getSecurityManager();
+			if(security != null){
+				security.checkLink(libCurlName);
+			}
+			System.load(libCurlName);
+		}
+
+	}
+
+	public static void loadLib(String libName){
+		if(getOsName().contains("windows")){
+			try {
+				//Windows is messy, so I am hard-coding this here ....
+				//This is only valid when called from Rdsamp.java. No other class should call
+				//this (if it does, input arg is ignored)
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "\\bin\\"  +"libcurl-4.dll");
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "\\bin\\"  +"wfdb-10.5.dll");
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "\\bin\\"  + "librdsampjni.exe.exe" );
+			} catch (UnsatisfiedLinkError e) {
+				System.err.println("Native code library failed to load.\n" + e);
+				System.exit(1);
+			}
+
+		}else if(getOsName().contains("mac")){
+			try{
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "/bin/" + libName + ".dylib");
+			}catch (Exception e){
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "/bin/lib" + libName + ".dylib");
+			}
+		}else{
+			//Default to Linux
+			try{
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "/lib64/" + libName + ".so");
+			}catch(Exception e){
+				System.load(SystemSettings.getWFDB_NATIVE_BIN(false) 
+						+ "/lib64/lib" + libName + ".so");
+			}
+		}
+	}
+
+
+	public static String getosArch(){
+		//Returns the JVM type
+		return System.getProperty("os.arch");
+	}
+
+	public static String getOsName(){
+		String osName=System.getProperty("os.name");
+		osName=osName.replace(" ","");
+		osName=osName.toLowerCase();
+		if(osName.startsWith("windows")){
+			osName="windows"; //Treat all Windows versions the same for now
+		}
+		return osName;
+	}
+
+	public static String getLD_PATH(boolean customArchFlag){
+		ProcessBuilder launcher = new ProcessBuilder();
+		Map<String,String> env = launcher.environment();
+		String LD_PATH="";
+		String WFDB_NATIVE_BIN=getWFDB_NATIVE_BIN(customArchFlag);
+		String osName=getOsName();
+		String tmp="", pathSep="";
+		String OsPathName;
+		if(osName.contains("windows")){
+			LD_PATH=env.get("Path");pathSep=";";
+			OsPathName="PATH";
+			tmp=WFDB_NATIVE_BIN + "bin" + pathSep + WFDB_NATIVE_BIN + "lib";
+		}else if(osName.contains("macosx")){
+			LD_PATH=env.get("DYLD_LIBRARY_PATH");pathSep=":";
+			OsPathName="DYLD_LIBRARY_PATH";
+			tmp=WFDB_NATIVE_BIN + "bin" + pathSep 
+					+ WFDB_NATIVE_BIN + "lib64" + pathSep 
+					+ WFDB_NATIVE_BIN + "lib";
+		}else{
+			LD_PATH=env.get("LD_LIBRARY_PATH");pathSep=":";
+			OsPathName="LD_LIBRARY_PATH";
+			tmp=WFDB_NATIVE_BIN + "bin" + pathSep 
+					+ WFDB_NATIVE_BIN + "lib64" + pathSep 
+					+ WFDB_NATIVE_BIN + "lib";
+		}
+
+		if(LD_PATH == null){
+			LD_PATH=tmp;
+		}else if(LD_PATH.indexOf(tmp) <0){
+			//Only add if path is not present already
+			LD_PATH=tmp+pathSep+LD_PATH;
+		}
+		return LD_PATH;
+	}
+
+	public static int getNumberOfProcessors(){
+		return Runtime.getRuntime().availableProcessors();
+	}
+
+	public static String getWFDB_JAVA_HOME(){
+		String packageDir = null;
+		try {
+			packageDir=URLDecoder.decode(
+					Wfdbexec.class.getProtectionDomain().getCodeSource().getLocation().getPath(),"utf-8");
+			packageDir = new File(packageDir).getPath();
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("Could not get path location of WFDB JAR file.");
+			e.printStackTrace();
+		}
+		int tmp = packageDir.lastIndexOf(fsep);
+		packageDir=packageDir.substring(0,tmp+1);
+		packageDir=packageDir.replace("file:","");
+		return packageDir.toString();
+	}
+
+	public synchronized static String getWFDB_NATIVE_BIN(boolean customArchFlag) {
+		String WFDB_NATIVE_BIN;
+		String WFDB_JAVA_HOME=getWFDB_JAVA_HOME();
+		//Set path to executables based on system/arch and customArchFlga
+		if(customArchFlag){
+			WFDB_NATIVE_BIN= WFDB_JAVA_HOME+ "nativelibs" + fsep + "custom"+ fsep;
+		}else{
+			WFDB_NATIVE_BIN= WFDB_JAVA_HOME+ "nativelibs" + fsep + 
+					getOsName().toLowerCase()+ fsep ;
+		}
+		return WFDB_NATIVE_BIN;
+	}
+
+}
